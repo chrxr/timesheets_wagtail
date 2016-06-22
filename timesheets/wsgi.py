f82ab357d7e2c34a0c192607031a465cdb4dc24a
Chris Rogers <chrxr@hotmail.co.uk>
2016-06-22 17:17:00 +0100

    hmm

--- timesheets/wsgi.py
+++ timesheets/wsgi.py
@@ -12,8 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timesheets.settings.production")
 
